0fb174753d63dfac3f84f242e12332b65608a08d
SSBeron <sultan.sagatov87@gmail.com>
2022-12-09 14:23:18 -0500

    removing parallax effect

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ gsap.from(featureDivs, {
   opacity: 0,
 });
 //Adding parallax effect\/
-const hero = document.getElementsByClassName("hero-img");
+// const hero = document.getElementsByClassName("hero-img");
 
-window.addEventListener("scroll", () => {
-  hero.style.backgroundPositionY = window.pageXOffset * 0.7 + "px";
-});
+// window.addEventListener("scroll", () => {
+//   hero.style.backgroundPositionY = window.pageXOffset * 0.7 + "px";
+// });
